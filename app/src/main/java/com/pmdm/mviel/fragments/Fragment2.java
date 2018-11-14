04e6fdef3bba5be4a2db64d4081bcda9d69674f1
mviel <mviel@florida-uni.es>
2018-11-14 14:56:23 +0100

    Comentaris afegits, i eliminació de codi referent als paràmetres que No reb el fragment

--- app/src/main/java/com/pmdm/mviel/fragments/Fragment2.java
+++ app/src/main/java/com/pmdm/mviel/fragments/Fragment2.java
@@ -11,26 +11,16 @@ import android.widget.TextView;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {interface
- * to handle interaction events.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
+ * Aquest Fragment només ha de permetre canviar el contingut
+ * de l'objecte TextView que conté.
+ * Com que aquest fragment no s'ha de comunicar amb l'activity que el conté, no cal que implemente
+ * cap interface. Per això les hem eliminat
+ * Tampoc rebrà cap paràmetre, per això eliminem el còdi que gestiona els paràmetres
  */
 public class Fragment2 extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
-
-    //Declare el TextView on mostraré el missatge
+   //Declare el TextView on mostrarà el missatge
     TextView tv_missatge;
 
 
@@ -39,30 +29,21 @@ public class Fragment2 extends Fragment {
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Modifiquem el mètode newInstance per a eliminar la gestió dels paràmetres,
+     * ja que aquest fragment no rep cap paràmetre
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment Fragment2.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static Fragment2 newInstance(String param1, String param2) {
         Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -70,6 +51,7 @@ public class Fragment2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =inflater.inflate(R.layout.fragment_fragment2, container, false);
+        //Enllacem l'objecte TextView amb l'element XML definit al layout fragment_fragment2.xml
         tv_missatge = v.findViewById(R.id.missatge);
         return v;
     }
@@ -87,7 +69,14 @@ public class Fragment2 extends Fragment {
         super.onDetach();
     }
 
+    /**
+     * Creem el mètode escriuMissatge que ens permetrà modificar el missatge que mostra el TextView
+     *@param  missatge Indica el missatge nou que ha de mostrar el fragment
+     *
+     *
+     */
     public void escriuMissatge(String missatge){
+        //Inserim el missatge en el TextView
         tv_missatge.setText(missatge);
     }
 }
